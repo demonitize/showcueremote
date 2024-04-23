e5b25ef0d05ceca6536a09e59328d7ceb4d976a8
DEMONITIZED BOI <42183865+demonitize@users.noreply.github.com>
2024-04-23 13:49:53 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const app = express();
 const balUtil = require('bal-util');
 const pathUtil = require('path');
 var outdated = false;
-
+var curv = require("./package.json").version;
+var newv = require("./package.json").version;
 
 
 app.use(express.static("public"));
 app.get("/", (request, response) => {
-	response.sendFile(__dirname + "/views/index.html");
+    if (!outdated) {
+	    response.status(200).sendFile(__dirname + "/views/index.html");
+    } else {
+	    response.status(202).sendFile(__dirname + "/views/index.html");
+    }
 });
 
 
@@ -28,8 +33,6 @@ function runCommand(codename) {
     //     address: cmd
     // })
 }
-
-
 
 app.get("/scs", async (request, response) => {
 	let req = request.query
@@ -69,18 +72,24 @@ app.get("/scs", async (request, response) => {
             runCommand(req.command);
             break;
 	}
-    await response.send({outdated: outdated, command: data.commands[req.command]});
+    await response.send({outdated: outdated, command: data.commands[req.command], newv, curv});
 })
 const listener = app.listen("80", () => {
 	console.log("Your app is listening on port " + listener.address().port);
 
     balUtil.packageCompare({
-        local: pathUtil.join(__dirname, '..', 'package.json'),
-        remote: 'https://raw.github.com/demonitize/showcueremote/master/package.json',
+        local: pathUtil.join(__dirname, './', 'package.json'),
+        remote: 'https://raw.githubusercontent.com/demonitize/showcueremote/main/package.json',
         newVersionCallback: function(details) {
             outdated = true;
+            newv = details.remote.version
             return console.log("There is a new version of " + details.local.name + " available, you should probably upgrade...\ncurrent version:  " + details.local.version + "\nnew version:      " + details.remote.version + "\ngrab it here:     " + details.remote.homepage);
+        },
+        sameVersionCallback: function(details) {
+            outdated = false;
+            return console.log("You are up to date!")
         }
+        
       });
 });
 
